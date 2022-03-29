12f209b5ff1c3d19e13ed42601ca47094d6647d5
Yehezkiel Wiradhika <tsukikagemaikaka1@gmail.com>
2022-03-29 16:39:13 +0700

    update final 19. expressjs with nodejs

--- 19. expressjs with nodejs/codes/calculator/app.js
+++ 19. expressjs with nodejs/codes/calculator/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const n1 = Number(req.body.num1);
+  const n2 = Number(req.body.num2);
+  const ans = n1 + n2;
+  res.send("result = " + ans);
 });
 
 app.listen(3000, function () {
